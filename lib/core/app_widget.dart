16b62c0e6f1f49b67af3d288ea9751b039f7e2e2
mathlouly <mathlouly@gmail.com>
2021-04-23 14:10:30 -0300

    First Update Code

---
+++ lib/core/app_widget.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "DevQuiz",
+      home: Container(),
+    );
+  }
+}
